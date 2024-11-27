6563b9a12a48f2d27d5523d045dcfda69feb762b
nikitam9111 <nikitamahangade9@gmail.com>
2024-11-27 21:11:49 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+package nov4;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1;i<=100;i=i+2)
+		{
+		sum=sum+i;
+	
+	}
+		System.out.println("The sum of even numbers "+sum);
+	}
+}
+		
+	
