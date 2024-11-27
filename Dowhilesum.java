60106be1813cd8152bbeac41adcd621750846692
nikitam9111 <nikitamahangade9@gmail.com>
2024-11-27 21:14:45 +0530

    Add files via upload

---
+++ Dowhilesum.java
@@ -0,0 +1,24 @@
+package nov4;
+
+public class Dowhilesum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    
+		
+		int i=1;
+		int sum=0;
+	    do{
+	    	if(i%2!=0){
+	    		sum+=i;
+	    		}
+	    	i++;
+	    }while(i<=100);	
+	    System.out.println("the sum of even number between 1 to 100 is " +sum);
+	    	}
+	    }
+
+
+	
+
+
